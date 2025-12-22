45c77de59b92b8b284cd58c403b341e19ee19c67
Akarshl <120399764+Akarshl@users.noreply.github.com>
2025-12-22 11:40:23 -0800

    Delete terraform directory

--- terraform/outputs.tf
+++
@@ -1,3 +0,0 @@
-output "ec2_public_ip" {
-  value = aws_instance.app_server.public_ip
-}
--- terraform/variables.tf
+++
@@ -1,4 +0,0 @@
-variable "aws_region" { default = "us-east-1" }
-variable "instance_name" {}
-variable "instance_type" {}
-variable "public_key_data" {}
